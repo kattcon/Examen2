cb22f792866a927dcd2bfe2dd364a958a373d6ff
Kate <kathii.conii@gmail.com>
2026-07-06 20:33:39 -0600

    se realiza un pequeño cambio en la BD por la refactorización

--- ExamTwo/ExamTwo/Controllers/Database.cs
+++ ExamTwo/ExamTwo/Controllers/Database.cs
@@ -2,7 +2,7 @@
 {
     public class Database
     {
-        public Dictionary<string, int> keyValues = new Dictionary<string, int>
+        public Dictionary<string, int> CoffeeQuantities = new Dictionary<string, int>
         {
             { "Americano", 10 },
             { "Cappuccino", 8 },
@@ -10,7 +10,7 @@
             { "Mocaccino", 15}
         };
 
-        public Dictionary<string, int> keyValues2 = new Dictionary<string, int>
+        public Dictionary<string, int> CoffeePricesInCents = new Dictionary<string, int>
         {
             { "Americano", 950 },
             { "Cappuccino", 1200 },
@@ -18,7 +18,7 @@
             { "Mocaccino", 1500}
         };
 
-        public Dictionary<int, int> keyValues3 = new Dictionary<int, int>
+        public Dictionary<int, int> CoinInventory = new Dictionary<int, int>
         {
             { 500, 20 },
             { 100, 30 },
