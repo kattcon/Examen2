ece9e2f9f36d94c57dce5d6a971924ad5b28cb7f
Kate <kathii.conii@gmail.com>
2026-07-06 20:53:38 -0600

    Se añade la segunda prueba unitaria

--- ExamTwo/UnitTesting/UnitTestingCoffeeMachine.cs
+++ ExamTwo/UnitTesting/UnitTestingCoffeeMachine.cs
@@ -23,4 +23,24 @@ public class UnitTestingCoffeeMachine
         var badRequest = Assert.IsType<BadRequestObjectResult>(result.Result);
         Assert.Equal("Orden vacía.", badRequest.Value);
     }
+
+        [Fact]
+    public void BuyCoffee_InsufficientPayment_ReturnsBadRequest()
+    {
+        // Arrange
+        var db = new Database();
+        var controller = new CoffeeMachineController(db);
+        var request = new OrderRequest
+        {
+            Order = new Dictionary<string, int> { { "Americano", 1 } },
+            Payment = new Payment { TotalAmount = 0 }
+        };
+
+        // Act
+        var result = controller.BuyCoffee(request);
+
+        // Assert
+        var badRequest = Assert.IsType<BadRequestObjectResult>(result.Result);
+        Assert.Equal("Dinero insuficiente.", badRequest.Value);
+    }
 }
